285721572688d93a70f48c92a64dd09cc355e8b1
Samundradk <48338968+Samundradk@users.noreply.github.com>
2019-05-27 20:45:11 +0930

    Add files via upload

---
+++ Design sprint 1/js/myscript.js
@@ -0,0 +1,10 @@
+function toggleSidebar(){
+  document.getElementById("sidebar").classList.toggle('active');
+}
+function on() {
+  document.getElementById("navigation").style.display = "block";
+}
+
+function off() {
+  document.getElementById("navigation").style.display = "none";
+}
